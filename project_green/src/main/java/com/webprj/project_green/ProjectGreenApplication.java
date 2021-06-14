653b8f96fdf3cae9ebe9a72702dc464c7538d548
yoondev83 <yoondev83@gmail.com>
2021-06-14 14:59:57 +0900

    기본 구성 컨트롤러 테스트 함.

---
+++ project_green/src/main/java/com/webprj/project_green/ProjectGreenApplication.java
@@ -0,0 +1,13 @@
+package com.webprj.project_green;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGreenApplication.class, args);
+	}
+
+}
---
+++ project_green/src/test/java/com/webprj/project_green/ProjectGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webprj.project_green;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
