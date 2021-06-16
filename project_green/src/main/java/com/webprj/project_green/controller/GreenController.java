7bff122d2f3686e58b415469aee90d68d4588301
yoondev83 <yoondev83@gmail.com>
2021-06-16 10:36:13 +0900

    메뉴 이동 오류 수정. 회원가입 정규식 추가 게시판 출력 구현.

--- project_green/src/main/java/com/webprj/project_green/controller/GreenController.java
+++ project_green/src/main/java/com/webprj/project_green/controller/GreenController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.webprj.project_green.dto.BoardDto;
 import com.webprj.project_green.service.BoardService;
