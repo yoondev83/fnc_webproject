4dc03b5e8695505f7cb3233040094f3d188c8e17
SangIl-fnino <mosang1230@naver.com>
2021-06-17 17:48:52 +0900

    좋아요 기능 완료!

--- project_green/src/main/java/com/webprj/project_green/service/LikeService.java
+++ project_green/src/main/java/com/webprj/project_green/service/LikeService.java
@@ -7,7 +7,8 @@ import javax.servlet.http.HttpSession;
 import com.webprj.project_green.dto.CustomDto;
 
 public interface LikeService {
-	public Map<String, Object> getArticleLikeAvailable(int boardnum, String loginedMemberId);
-	public Map<String, Object> likeArticle(int boardnum, String loginedMemberId);
+	public Map<String, Object> getArticleLikeAvailable(int boardnum, String loginedMemberId , boolean update);
+	public void likeArticle(int boardnum, String loginedMemberId);
 	public void unlikeArticle(int boardnum, String loginedMemberId);
+	public int getRecommendPoint(int boardnum);
 }
